c7490017c82e3f6b712cfd0e8e54d3d2616128a0
Marce Ramirez <marceram87@gmail.com>
2020-01-23 10:49:53 +0100

    Updates

---
+++ precache-manifest.8a75787aec362792eb31db0b72bf36a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9128b52abb9d41998e6c85c5ecfda75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3463e2f2fe7b518c23a",
+    "url": "/static/css/main.b934ce9d.chunk.css"
+  },
+  {
+    "revision": "1243d664bb37732c5068",
+    "url": "/static/js/2.8690167e.chunk.js"
+  },
+  {
+    "revision": "b85c2b7a86a8fe2196cb7a9cd5db10f2",
+    "url": "/static/js/2.8690167e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3463e2f2fe7b518c23a",
+    "url": "/static/js/main.1f44e657.chunk.js"
+  },
+  {
+    "revision": "b9560e114637e889b6ad",
+    "url": "/static/js/runtime-main.f3afa842.js"
+  }
+]);
